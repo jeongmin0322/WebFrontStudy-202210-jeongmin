1265011982f5998194ef212e0bdbdfca69b712db
jeongmin0322 <minorsty@naver.com>
2022-11-23 21:51:52 +0900

    java script, let, 익명함수, 화살표함수,

---
+++ js/class1.js
@@ -0,0 +1,31 @@
+// let user = {
+//     username: "junil",
+//     password: "1234"
+// };
+
+
+class User {
+    username;
+    password;
+    name;
+    email;
+}
+
+class UserMain {
+    main() {
+        let user = new User();
+
+        user.username = "junil";
+        user.password = "1234";
+        user.name = "김준일";
+        user.email = "skjil1218@gmail.com";
+
+        console.log(user);
+    }
+}
+
+window.onload = () => {
+    let UserMain = new UserMain();
+
+    UserMain.main();
+}
