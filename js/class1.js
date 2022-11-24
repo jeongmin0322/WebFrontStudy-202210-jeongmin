ce532b95cd2711ac1d13ca9f94305a4ee2dc0c9f
jeongmin0322 <minorsty@naver.com>
2022-11-24 21:45:45 +0900

    DOM에서 이벤트처리하기

--- js/class1.js
+++ js/class1.js
@@ -9,23 +9,48 @@ class User {
     password;
     name;
     email;
+
+    constructor(username, password,name,email) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    printName() {
+        console.log(this.name+"님의 정보")
+    }
 }
 
-class UserMain {
-    main() {
-        let user = new User();
+// 싱글톤
+class UserMain { 
+    static #instance = null;
+    static getInstance() {
+        if(this.#instance == null){
+            this.#instance = new UserMain();
+        }
+        return this.#instance;
+    }
 
-        user.username = "junil";
-        user.password = "1234";
-        user.name = "김준일";
-        user.email = "skjil1218@gmail.com";
+    main() {
+        let user = new User("junil","1234","김준일","skjil1218@gmail.com");
+
+        // user.username = "junil";
+        // user.password = "1234";
+        // user.name = "김준일";
+        // user.email = "skjil1218@gmail.com";
 
         console.log(user);
+
+        localStorage.username = user.username;
+        console.log("username:"+localStorage.username); //페이지 이동되어도 데이터가 살아있기때문에 전역변수라고 볼 수 있음
+
+        user.printName();
     }
 }
 
 window.onload = () => {
-    let UserMain = new UserMain();
+    let userMain = UserMain.getInstance();
 
-    UserMain.main();
+    userMain.main();
 }
